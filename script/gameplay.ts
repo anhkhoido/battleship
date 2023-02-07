46e8d4ffddde9b3466aa6f85acba4e92e2988d0c
anhkhoido <anhkhoido@gmail.com>
2023-02-07 11:23:06 -0500

    Added functions and variable in gameplay.ts

--- script/gameplay.ts
+++ script/gameplay.ts
@@ -1,25 +1,25 @@
 var spanTagWithCurrentYear = document.getElementById('span_current_year_id') as HTMLSpanElement;
 spanTagWithCurrentYear.innerText = new Date().getFullYear().toString();
 var table = document.getElementById('table_game') as HTMLDivElement;
-var gameBoard : HTMLSpanElement[][] = new Array<Array<HTMLSpanElement>>;
+var opposingSide = document.getElementById('opposing_side') as HTMLDivElement;
 
-class Ship {
-    constructor(public hitPoints: number, public xAxis : number, public yAxis : number) {}
+var gameBoard : HTMLSpanElement[][] = new Array<Array<HTMLSpanElement>>;
+var opposingBoard : HTMLSpanElement[][] = new Array<Array<HTMLSpanElement>>;
+
+enum PositionType {
+    HORIZONTAL,
+    VERTICAL,
 }
 
-var myFleet : Ship[] = createFleet();
-var enemyFleet : Ship[] = createFleet();
+class Ship {
+    constructor(public hitPoints: number, public xAxis : number, public yAxis : number, public positionType : PositionType) {}
+}
+
+// Player's board
 createBoard();
 displayBoard();
 
-function createFleet() : Ship[] {
-    var carrier = new Ship(5, 0, 0);
-    var battleship = new Ship(4, 0, 0);
-    var cruiser = new Ship(3, 0, 0);
-    var submarine = new Ship(3, 0, 0);
-    var destroyer = new Ship(2, 0, 0);
-    return new Array(carrier, battleship, cruiser, submarine, destroyer);
-}
+createOpposingBoard();
 
 function createBoard() : void {
     for (var i = 0; i < 11; i++) {
@@ -35,8 +35,9 @@ function createBoard() : void {
                 }
             } else if (i !== 0 && j === 0) {
                 mySpan.innerText = i.toString();
-            } else {
-                mySpan.innerHTML = '&nbsp;';
+            } else if (i >= 1 && j >= 1) {
+                var temp : string = String.fromCharCode(65 + (j - 1)) + ", " + i.toString(); 
+                mySpan.innerText = temp;
             }
             rowOfSpans.push(mySpan);
         }
@@ -50,6 +51,39 @@ function displayBoard() : void {
             table.appendChild(gameBoard[i][j]);
             if (j == 10) {
                 table.appendChild(document.createElement('br'));
+            }
+        }
+    }
+}
+
+function createOpposingBoard() : void {
+    for (var i = 0; i < 11; i++) {
+        var rowOfSpans : HTMLSpanElement[] = new Array<HTMLSpanElement>;
+        for (var j = 0; j < 11; j++) {
+            let mySpan = document.createElement('span') as HTMLSpanElement;
+            mySpan.className = 'grid_square';
+            if (i === 0) {
+                if (j === 0) {
+                    mySpan.innerHTML = '&nbsp;';
+                } else {
+                    mySpan.innerText = String.fromCharCode(65 + (j - 1));
+                }
+            } else if (i !== 0 && j === 0) {
+                mySpan.innerText = i.toString();
+            } else if (i >= 1 && j >= 1) {
+                var temp : string = String.fromCharCode(65 + (j - 1)) + ", " + i.toString(); 
+                mySpan.innerText = temp;
+            }
+            rowOfSpans.push(mySpan);
+        }
+        opposingBoard.push(rowOfSpans);
+    }
+
+    for (var i = 0; i < opposingBoard.length; i++) {
+        for (var j = 0; j < opposingBoard[i].length; j++) {
+            opposingSide.appendChild(opposingBoard[i][j]);
+            if (j == 10) {
+                opposingSide.appendChild(document.createElement('br'));
             }
         }
     }
