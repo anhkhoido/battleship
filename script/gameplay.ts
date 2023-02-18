92d290b51c76fc039c0745dfa48a926357876a32
anhkhoido <anhkhoido@gmail.com>
2023-02-18 18:35:04 -0500

    Re-wrote random placement of ships.

--- script/gameplay.ts
+++ script/gameplay.ts
@@ -1,10 +1,10 @@
 var spanTagWithCurrentYear = document.getElementById('span_current_year_id') as HTMLSpanElement;
 spanTagWithCurrentYear.innerText = new Date().getFullYear().toString();
 var table = document.getElementById('table_game') as HTMLDivElement;
-var opposingSide = document.getElementById('opposing_side') as HTMLDivElement;
+var arrayAllCoordinatesOfShips = new Array<Array<string>>;
+var arrayOfShips = new Array<Ship>;
 
 var gameBoard : HTMLSpanElement[][] = new Array<Array<HTMLSpanElement>>;
-var opposingBoard : HTMLSpanElement[][] = new Array<Array<HTMLSpanElement>>;
 
 enum PositionType {
     HORIZONTAL = "Horizontal",
@@ -12,15 +12,17 @@ enum PositionType {
 }
 
 class Ship {
-    constructor(public hitPoints: number, public positionType : PositionType, public letterAxis : string, public yAxis : number) {}
+    constructor(public hitPoints: number, public positionType : PositionType, public coordinates : Array<String>) {}
 }
 
 // Player's board
 createBoard();
 displayBoard();
-
-createOpposingBoard();
 placeShipsOfEnemy();
+
+arrayAllCoordinatesOfShips.forEach(function(value) {
+    alert(value);
+});
 
 function placeShipsOfEnemy() : void {
     const generateRandomNumber = (min: number, max: number) => {
@@ -32,49 +34,71 @@ function placeShipsOfEnemy() : void {
     var randomPositionForDestroyer : number = generateRandomNumber(1, 2);
     var destroyer = createEnemyShip(5, 
         randomPositionForDestroyer == 1 ? PositionType.HORIZONTAL : PositionType.VERTICAL);
+
+    var randomPositionForBattleShip : number = generateRandomNumber(1, 2);
+    var battleShip = createEnemyShip(4, randomPositionForBattleShip == 1 ? PositionType.HORIZONTAL : PositionType.VERTICAL);
+
+    var randomPositionForCruiser : number = generateRandomNumber(1, 2);
+    var cruiser = createEnemyShip(3, randomPositionForCruiser == 1 ? PositionType.HORIZONTAL : PositionType.VERTICAL);
+
+    var randomPositionSubmarine : number = generateRandomNumber(1, 2);
+    var submarine = createEnemyShip(3, randomPositionSubmarine == 1 ? PositionType.HORIZONTAL : PositionType.VERTICAL);
+
+    var randomPositionPatrolBoat : number = generateRandomNumber(1, 2);
+    var patrolBoat = createEnemyShip(2, randomPositionPatrolBoat == 1 ? PositionType.HORIZONTAL : PositionType.VERTICAL);
+
+    arrayOfShips.push(destroyer);
+    arrayOfShips.push(battleShip);
+    arrayOfShips.push(cruiser);
+    arrayOfShips.push(submarine);
+    arrayOfShips.push(patrolBoat);
 }
 
 function createEnemyShip(hitPoints : number, positionType : PositionType) : Ship {
-    var yPosition : number = 1;
-
     const generateRandomNumber = (min: number, max: number) => {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    // Starting position
-    var charCode : number = generateRandomNumber(1, 10 - hitPoints);
-    var letter : string = String.fromCharCode(65 + charCode);
-    var numberOnYAxis : number = generateRandomNumber(1, 10);
 
     var arrayOfCoordinates : string[] = new Array();   
+    var ship = new Ship(hitPoints, positionType, new Array());
+    var foundDuplicate : boolean = false;
+    var flattennedArrayOfCoordinatesAllShips = arrayAllCoordinatesOfShips.reduce((accumulator, value) => accumulator.concat(value), []);
 
-    
-    var ship = new Ship(hitPoints, positionType, letter, yPosition);
-    var childNodesOfOpposingSide = opposingSide.childNodes;
-
-    function excludeBrTags(element : HTMLElement) : boolean {
-        return element.tagName !== 'BR';
-    }
-
-    var arrayOfChildrenForOpposingSide : any = Array.prototype
-        .slice
-        .call(childNodesOfOpposingSide)
-        .filter(excludeBrTags);
     if (PositionType.HORIZONTAL === ship.positionType) {
+        var charCode : number = generateRandomNumber(1, 10 - hitPoints - 1);
+        var letter : string = String.fromCharCode(65 + charCode);
+        var numberOnYAxis : number = generateRandomNumber(1, 10);
         do {
             charCode++;
             letter = String.fromCharCode(65 + charCode);
-            arrayOfCoordinates.push(letter + numberOnYAxis);
-            hitPoints--;
-        } while (hitPoints !== 0);
+            foundDuplicate = flattennedArrayOfCoordinatesAllShips.indexOf(letter + numberOnYAxis) >= 0;
+            if (!foundDuplicate) {
+                ship.coordinates.push(letter + numberOnYAxis);
+                arrayOfCoordinates.push(letter + numberOnYAxis);
+                hitPoints--;
+            }
+        } while (hitPoints !== 0 && !foundDuplicate);
     } else if (PositionType.VERTICAL === ship.positionType) {
+        var charCode : number = generateRandomNumber(1, 10 - hitPoints - 1);
+        var letter : string = String.fromCharCode(65 + charCode);
+        var numberOnYAxis : number = generateRandomNumber(1, 10 - hitPoints);
         do {
-            hitPoints--;
-        } while (hitPoints !== 0);
+            numberOnYAxis++;
+            letter = String.fromCharCode(65 + charCode);
+            foundDuplicate = flattennedArrayOfCoordinatesAllShips.indexOf(letter + numberOnYAxis) >= 0;
+            if (!foundDuplicate) {
+                ship.coordinates.push(letter + numberOnYAxis);
+                arrayOfCoordinates.push(letter + numberOnYAxis);
+                hitPoints--;
+            }
+        } while (hitPoints !== 0 && !foundDuplicate);
     }
-    alert("Array of coordinates: " + arrayOfCoordinates);
-    return ship;
+    if (!foundDuplicate) {
+        arrayAllCoordinatesOfShips.push(arrayOfCoordinates);
+    }
+    return !foundDuplicate ? ship : createEnemyShip(hitPoints, positionType);
 }
 
 function createBoard() : void {
@@ -92,7 +116,7 @@ function createBoard() : void {
             } else if (i !== 0 && j === 0) {
                 mySpan.innerText = i.toString();
             } else if (i >= 1 && j >= 1) {
-                var temp : string = String.fromCharCode(65 + (j - 1)) + "," + i.toString(); 
+                var temp : string = String.fromCharCode(65 + (j - 1)) + i.toString(); 
                 mySpan.innerText = temp;
                 mySpan.title = temp;
             }
@@ -108,40 +132,6 @@ function displayBoard() : void {
             table.appendChild(gameBoard[i][j]);
             if (j == 10) {
                 table.appendChild(document.createElement('br'));
-            }
-        }
-    }
-}
-
-function createOpposingBoard() : void {
-    for (var i = 0; i < 11; i++) {
-        var rowOfSpans : HTMLSpanElement[] = new Array<HTMLSpanElement>;
-        for (var j = 0; j < 11; j++) {
-            let mySpan = document.createElement('span') as HTMLSpanElement;
-            mySpan.className = 'grid_square';
-            if (i === 0) {
-                if (j === 0) {
-                    mySpan.innerHTML = '&nbsp;';
-                } else {
-                    mySpan.innerText = String.fromCharCode(65 + (j - 1));
-                }
-            } else if (i !== 0 && j === 0) {
-                mySpan.innerText = i.toString();
-            } else if (i >= 1 && j >= 1) {
-                var temp : string = String.fromCharCode(65 + (j - 1)) + "," + i.toString(); 
-                mySpan.title = temp;
-                mySpan.innerHTML = '&nbsp;';
-            }
-            rowOfSpans.push(mySpan);
-        }
-        opposingBoard.push(rowOfSpans);
-    }
-
-    for (var i = 0; i < opposingBoard.length; i++) {
-        for (var j = 0; j < opposingBoard[i].length; j++) {
-            opposingSide.appendChild(opposingBoard[i][j]);
-            if (j == 10) {
-                opposingSide.appendChild(document.createElement('br'));
             }
         }
     }
