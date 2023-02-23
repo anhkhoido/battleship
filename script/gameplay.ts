b509f60e4f4ed0c36774a23254c6fc3668433c53
anhkhoido <anhkhoido@gmail.com>
2023-02-23 16:54:13 -0500

    Finalized scoring system.

--- script/gameplay.ts
+++ script/gameplay.ts
@@ -3,11 +3,14 @@ spanTagWithCurrentYear.innerText = new Date().getFullYear().toString();
 var table = document.getElementById('table_game') as HTMLDivElement;
 var arrayAllCoordinatesOfShips = new Array<Array<string>>;
 var arrayOfShips = new Array<Ship>;
-let resetButton = document.getElementById('reset_button') as HTMLButtonElement;
 
 var gameBoard : HTMLSpanElement[][] = new Array<Array<HTMLSpanElement>>;
 var scoreHeading = document.getElementById('score') as HTMLHeadingElement;
 var actualScore : number = 0;
+var actualNumberOfSunkenShips : number = 0;
+var shotsFired : number = 0;
+var sunkenShipsHeading = document.getElementById('sunken_ships') as HTMLHeadingElement;
+var shotsFiredHeading = document.getElementById('shots_fired') as HTMLHeadingElement;
 
 let explosionSound = document.getElementById('explosion') as HTMLAudioElement;
 let waterSplash = document.getElementById('waterSplash') as HTMLAudioElement;
@@ -18,7 +21,7 @@ enum PositionType {
 }
 
 class Ship {
-    constructor(public hitPoints: number, public positionType : PositionType, public coordinates : Array<String>) {}
+    constructor(public hitPoints: number, public positionType : PositionType, public coordinates : Array<String>, public isSunken : boolean) {}
 }
 
 // Player's board
@@ -64,7 +67,7 @@ function createEnemyShip(hitPoints : number, positionType : PositionType) : Ship
     }
 
     var arrayOfCoordinates : string[] = new Array();   
-    var ship = new Ship(hitPoints, positionType, new Array());
+    var ship = new Ship(hitPoints, positionType, new Array(), false);
     var foundDuplicate : boolean = false;
     var flattennedArrayOfCoordinatesAllShips = arrayAllCoordinatesOfShips.reduce((accumulator, value) => accumulator.concat(value), []);
     var remainingHitPointsForDoWhile = hitPoints;
@@ -128,21 +131,44 @@ function createBoard() : void {
                 mySpan.addEventListener("click", function() {
                     var result = flattennedArrayOfCoordinatesAllShips.indexOf(mySpan.title);
                     if (result >= 0 && mySpan.innerText !== 'O') {
+                        updateNumberOfSunkenShips(mySpan.innerText);
                         mySpan.innerText = 'O';
                         mySpan.style.backgroundColor = '#FF0808';
                         explosionSound.play();
                         actualScore++;
+                        shotsFired++;
                         scoreHeading.innerText = 'Your score: ' + actualScore;
+                        shotsFiredHeading.innerText = 'Shots fired: ' + shotsFired;
                     } else if (mySpan.innerText !== 'X' && mySpan.innerText !== 'O' && result === -1) {
                         mySpan.innerText = 'X';
                         waterSplash.play();
+                        shotsFired++;
+                        shotsFiredHeading.innerText = 'Shots fired: ' + shotsFired;
                     }
+
+                    actualNumberOfSunkenShips = arrayOfShips.map(s => s.isSunken).filter(condition => condition).length;
+                    sunkenShipsHeading.innerText = 'Sunken ships: ' + actualNumberOfSunkenShips + "/5";
                 });
             }
             rowOfSpans.push(mySpan);
         }
         gameBoard.push(rowOfSpans);
     }
+}
+
+function updateNumberOfSunkenShips(input : string) : void {
+    var numberOfHitsTaken : number = 0;
+    arrayOfShips.forEach(function(ship) {
+        for (var i = 0; i < ship.coordinates.length; i++) {
+            if (ship.coordinates[i] === input) {
+                ship.coordinates[i] = 'O';
+            }
+        }
+        numberOfHitsTaken = ship.coordinates.filter(value => value === 'O').length;
+        if (ship.hitPoints === numberOfHitsTaken) {
+            ship.isSunken = true;
+        }
+    });
 }
 
 function displayBoard() : void {
